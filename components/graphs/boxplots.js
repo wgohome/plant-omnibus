c168bc26d77453078368fba7540ccbd8f1a730d5
kevfo <75442525+kevfo@users.noreply.github.com>
2022-09-04 03:01:55 +0800

    Added a new folder to store gene graphs
    Parts of my code have been changed to work with NextJS and also for readability purposes

--- components/graphs/boxplots.js
+++ components/graphs/boxplots.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
-import Plot from "react-plotly.js";
-import { removeOutliers } from "../HelperFunctions/math.js";
-import { assignColors } from "../Data/genePageInfo.js";
+import { removeOutliers } from "../../utils/mathFunctions.js";
+import { groupColors, assignColors, imageOptions } from "../../utils/genePageFunctions.js";
+
+// Import Plot dynamically without server side rendering:
+import dynamic from 'next/dynamic';
+const Plot = dynamic(() => import('react-plotly.js'), { ssr : false })
 
 class GeneBoxplot extends React.Component {
   constructor(props) {
@@ -10,12 +13,11 @@ class GeneBoxplot extends React.Component {
       geneID: props.geneID,
       organs : props.names,
       boxData: props.data,
-      options: props.option,
-      colors: props.colors,     // Assuming that we do use this in the future, might somebody else want to pass in some other colors?
-      clicked: true,            // For undoing changes made by the event handler...
-      noOutliers: false,        // By default, assume that we want to show all points including outliers.
-      notched : false,          // Determines whether or not the box plot should be notched or not.
-      originalData : props.data // So that we can re-store the original data if need be.
+      options: imageOptions,
+      clicked: true,             // For undoing changes made by the event handler...
+      noOutliers: false,         // By default, assume that we want to show all points including outliers.
+      notched : false,           // Determines whether or not the box plot should be notched or not.
+      originalData : props.data, // So that we can re-store the original data if need be.
     }
   }
 
@@ -34,23 +36,27 @@ class GeneBoxplot extends React.Component {
       boxData : ((data) => {
         if (prevState.clicked) {
           for (let i = 0; i < data.length ; i++) {
+            let colorIndex = assignColors(data[i]['name']);
+
             data[i]['y'] = (prevState.noOutliers) ? removeOutliers(data[i]['y']) : prevState.originalData[i]['y'];
             data[i]['boxpoints'] = 'all';
             data[i]['jitter'] = 1;
             data[i]['pointpos'] = 0;
-            data[i]['line'] = { width : 0.6, color: `rgba(${prevState.colors[assignColors(data[i]['name'])].r}, ${prevState.colors[assignColors(data[i]['name'])].g}, ${prevState.colors[assignColors(data[i]['name'])].b}, 1)`};
-            data[i]['fillcolor'] = `rgba(${prevState.colors[assignColors(data[i]['name'])].r}, ${prevState.colors[assignColors(data[i]['name'])].g}, ${prevState.colors[assignColors(data[i]['name'])].b}, 0.1)`;
+            data[i]['line'] = { width : 0.6, color: `rgba(${groupColors[colorIndex].r}, ${groupColors[colorIndex].g}, ${groupColors[colorIndex].b}, 1)`};
+            data[i]['fillcolor'] = `rgba(${groupColors[colorIndex].r}, ${groupColors[colorIndex].g}, ${groupColors[colorIndex].b}, 0.1)`;
             data[i]['marker'] = {opacity : 1}
             data[i]['notched'] = prevState.notched;
             data[i]['orientation'] = prevState.orientation ? 'h' : 'v';
           }
         } else {
           for (let i = 0; i < data.length ; i++) {
+            let colorIndex = assignColors(data[i]['name']);
+
             data[i]['boxpoints'] = undefined;
             data[i]['jitter'] = undefined;
             data[i]['pointpos'] = undefined;
-            data[i]['line'] = { width : 2, color: `rgba(${prevState.colors[assignColors(data[i]['name'])].r}, ${prevState.colors[assignColors(data[i]['name'])].g}, ${prevState.colors[assignColors(data[i]['name'])].b}, 1)` };
-            data[i]['fillcolor'] = `rgba(${prevState.colors[assignColors(data[i]['name'])].r}, ${prevState.colors[assignColors(data[i]['name'])].g}, ${prevState.colors[assignColors(data[i]['name'])].b}, 0.5)`;
+            data[i]['line'] = { width : 2, color: `rgba(${groupColors[colorIndex].r}, ${groupColors[colorIndex].g}, ${groupColors[colorIndex].b}, 1)` };
+            data[i]['fillcolor'] = `rgba(${groupColors[colorIndex].r}, ${groupColors[colorIndex].g}, ${groupColors[colorIndex].b}, 0.5)`;
             data[i]['opacity'] = 1;
             data[i]['marker'] = {opacity: 0};
             data[i]['notched'] = prevState.notched;
@@ -77,16 +83,18 @@ class GeneBoxplot extends React.Component {
         <div className = "pt-3">
           <div className="flex">
             <p className = 'pr-3 font-medium'> Viewing options: </p>
-            <div className="inline-flex shadow-md hover:shadow-lg focus:shadow-lg" role="group">
+            <div className="inline-flex shadow-md hover:shadow-lg focus:shadow-lg space-x-1" role="group">
               <button type="button" onClick = {this.handleClick} className="rounded-l inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out"> {this.state.noOutliers ? "Outliers removed!" : "Remove outliers"} </button>
               <button type="button" onClick = {this.handleNotch} className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase hover:bg-blue-700 focus:bg-blue-700 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out"> {this.state.notched ? "Remove notches" : "Add notches"} </button>
             </div>
           </div>
         </div>
         {this.state.noOutliers &&
-          <div className = "container">
-            <br/>
-            <p className = "text-primary" style = {{paddingLeft : "43px"}}> Outliers removed!  Refresh page to see changes! </p>
+          <div>
+          <br/>
+          <div className = "container bg-white border border-solid border-gray-800 rounded">
+            <p className = "text-primary py-2 px-2"> Outliers removed!  Click on the plot to see the change! </p>
+          </div>
           </div>
         }
       </div>
