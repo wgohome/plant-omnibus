e4ae8e27855cdf1cfa56a99200be1b7e7ae02fad
William Goh <wirriamm@gmail.com>
2022-09-26 17:44:54 +0800

    feat: made custom link component

--- components/search/ProteinSearchBox.tsx
+++ components/search/ProteinSearchBox.tsx
@@ -1,10 +1,13 @@
 import React from "react"
+import TextLink from "../atomic/TextLink"
 
 const ProteinSearchBox = ({submitSearchQuery}) => {
   const handleSubmit = (event) => {
     event.preventDefault()
     submitSearchQuery(event.target.proteinSeq.value)
   }
+
+  const sampleSeq = "MEEENQKSHRVSRKDQSGSHWSQGADEEPRARCSGKRCRSWAAAAIADCVALCCCPCAVVNIFTLAFVKVPWMIGRKCIGRGGPSKKRMKKINREDRFHHHHHHRRSAEMVSGGCCGGGDGDGEFDDHRFVVERDGSLTKEEAKTASLKEEEETRISARVEAERVWLELYQIGHLGFGRVSFTGIHQ"
 
   return (
     <form action="#" onSubmit={handleSubmit}>
@@ -18,13 +21,27 @@ const ProteinSearchBox = ({submitSearchQuery}) => {
             // defaultValue=""
           />
         </div>
-        <div className="my-3">
-          <button
-            type="submit"
-            className="text-xl text-plb-light bg-plb-green hover:bg-plb-dark-green focus:outline-none focus:ring-4 focus:ring-green-300 rounded-full px-6 py-3 text-center"
-          >
-            Search
-          </button>
+        <div className="flex my-3">
+          <div className="flex-grow">
+            <div className="">
+              <a className="hover:underline text-plb-green active:text-plb-red">
+                Load example protein sequence
+              </a>
+            </div>
+            <div className="mt-2">
+              <TextLink href="/search">
+                Search by gene identifiers instead
+              </TextLink>
+            </div>
+          </div>
+          <div className="">
+            <button
+              type="submit"
+              className="text-xl text-plb-light bg-plb-green hover:bg-plb-dark-green focus:outline-none focus:ring-4 focus:ring-green-300 rounded-full px-6 py-3 text-center"
+              >
+              Search
+            </button>
+          </div>
         </div>
       </div>
     </form>
