a86c9683269ef38b8b5e8f9b7f826c8df66156af
William Goh <wirriamm@gmail.com>
2022-10-12 20:50:39 +0800

    feat: interpro index page with tables of interpro annotations

---
+++ components/tables/InterproIndexTable.tsx
@@ -0,0 +1,75 @@
+import React from "react"
+
+import VirtualPaginatedTable from "./generics/VirtualPaginatedTable"
+import TextLink from "../atomic/TextLink"
+
+
+interface IProps {
+  initialGeneAnnotations: object[]
+  pageTotal: number
+}
+
+const InterproIndexTable: React.FC<IProps> = ({ initialGeneAnnotations, pageTotal }) => {
+  // Pagination state management
+  const [ gaPage, setGaPage ] = React.useState(initialGeneAnnotations)
+  const [ pageCount, setPageCount ] = React.useState(pageTotal)
+  const [ loading, setLoading ] = React.useState(false)
+  const fetchIdRef = React.useRef(0)
+
+  // Table columns
+  const columns = React.useMemo(() => [
+    {
+      Header: "PFAM ID",
+      accessor: "label",
+      Cell: ({ value }: { value: string }) => (
+        <TextLink href={`#`}>
+          {value}
+        </TextLink>
+      ),
+    },
+    {
+      Header: "Name",
+      accessor: "name",
+    },
+    {
+      Header: "GO Terms",
+      accessor: "details.go_terms",
+      Cell: ({ value: goTerms }: { value: object[] | "-" }) => (
+        <ul className="">
+          {
+            (goTerms.length === 0 || goTerms[0] === "-")
+              ? "-"
+              : goTerms.join(", ")
+          }
+        </ul>
+      )
+    },
+  ], [])
+
+  const fetchGaPage = React.useCallback(({ pageSize, pageIndex }: { pageSize: number; pageIndex: number }) => {
+    const fetchId = ++fetchIdRef.current
+    setLoading(true)
+    if (fetchId === fetchIdRef.current) {
+      fetch(`/api/interpro?pageIndex=${pageIndex}&pageSize=${pageSize}`)
+      .then(res => res.json())
+      .then((data) => {
+          setGaPage(data.geneAnnotations)
+          setPageCount(data.pageTotal)
+          setLoading(false)
+        })
+        .catch(err => console.log(err))
+    }
+  }, [])
+
+  return (
+    <VirtualPaginatedTable
+      columns={columns}
+      data={gaPage}
+      pageCount={pageCount}
+      loading={loading}
+      fetchData={fetchGaPage}
+    />
+  )
+}
+
+export default InterproIndexTable
---
+++ pages/api/interpro/index.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { getGeneAnnotationsPage } from '../../../utils/geneAnnotations'
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET":
+      try {
+        let { pageIndex, pageSize } = req.query
+        pageIndex = parseInt(pageIndex) || 0
+        pageSize = parseInt(pageSize) || parseInt(process.env.pageSize)
+        const genePage = await getGeneAnnotationsPage({
+          type: "INTERPRO",
+          pageIndex,
+          pageSize,
+        })
+        if (pageIndex < 0 || pageIndex > genePage.pageTotal) {
+          res.status(422).json({
+            error: `${pageIndex} is an invalid pageIndex`
+          })
+        }
+        res.status(200).json(genePage)
+      } catch (error) {
+        console.log(error)
+        res.status(422).json({ error: "invalid query" })
+      }
+      break
+    default:
+      console.log("Method not available for this endpoint")
+      res.status(405).json({error: "Method not available for this endpoint"})
+  }
+}
