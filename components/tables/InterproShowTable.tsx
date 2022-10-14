525856b609e38247640b41cbec7e77faee7cc316
William Goh <wirriamm@gmail.com>
2022-10-14 12:08:04 +0800

    feat: interpro show page with table

---
+++ components/tables/InterproShowTable.tsx
@@ -0,0 +1,48 @@
+import React from "react"
+
+import LocalPaginatedTable from "./generics/LocalPaginatedTable"
+import TextLink from "../atomic/TextLink"
+
+const InterproShowTable = ({ data }) => {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Species",
+        accessor: "species.name", // TODO
+        Cell: ({ value, row }) => (
+          <TextLink href={`/species/${row.values.tax}`}>
+            {value}
+          </TextLink>
+        ),
+      },
+      {
+        Header: "Tax ID",
+        accessor: "tax",
+      }, // To hide, but needed to access the value for species url
+      {
+        Header: "Genes",
+        accessor: "genes",
+        Cell: ({ value, row }) => (
+          value.map((gene, i) => (
+            <span className="" key={i}>
+              <TextLink href={`/species/${row.values.tax}/genes/${gene.label}`}>
+                {gene.label}
+              </TextLink>
+              {",  "}
+            </span>
+          ))
+        ),
+      },
+    ], []
+  )
+
+  return (
+    <LocalPaginatedTable
+      columns={columns}
+      data={data}
+      hiddenColumns={["tax"]}
+    />
+  )
+}
+
+export default InterproShowTable
--- pages/interpro/[label].tsx
+++ pages/interpro/[label].tsx
@@ -5,20 +5,24 @@ import { useRouter } from "next/router"
 
 import Layout from "../../components/Layout"
 import Header1 from "../../components/atomic/texts/Header1"
+import InterproShowTable from "../../components/tables/InterproShowTable"
+import { getOneGeneAnnotation } from "../../utils/geneAnnotations"
 
 export const getServerSideProps: GetServerSideProps = async ({ params, query }) => {
+  const geneAnnotation = await getOneGeneAnnotation({ type: "INTERPRO", label: params.label })
+
   return {
     props: {
-
+      geneAnnotation: JSON.parse(JSON.stringify(geneAnnotation))
     }
   }
 }
 
 interface IProps {
-
+  geneAnnotation: object
 }
 
-const InterproShowPage: NextPage<IProps> = ({  }) => {
+const InterproShowPage: NextPage<IProps> = ({ geneAnnotation }) => {
   const router = useRouter()
   const label = router.query.label
 
@@ -29,6 +33,14 @@ const InterproShowPage: NextPage<IProps> = ({  }) => {
       </Head>
 
       <Header1>PFAM {label}</Header1>
+      <p>{geneAnnotation.name}</p>
+      <p>
+        GO Terms: {geneAnnotation.details.go_terms.join(", ")}
+      </p>
+
+      {JSON.stringify(geneAnnotation.gene_annotation_buckets[0], undefined, 2)}
+
+      <InterproShowTable data={geneAnnotation.gene_annotation_buckets} />
     </Layout>
   )
 }
