304bcb831f04100fdc3c1db895a42de0ef3525fb
William Goh <wirriamm@gmail.com>
2022-10-14 12:07:23 +0800

    feat: table footer generic component

---
+++ components/tables/generics/PageStatusFooter.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+interface IProps {
+  pageIndex: number  // 0-indexed
+  pageLength: number
+  loading?: boolean  // Only for virtual tables
+}
+
+const PageStatusFooter: React.FC<IProps> = ({ pageIndex, pageLength, loading }) => {
+  return (
+    <div className="px-3">
+      {loading ? (
+        <span>Loading ...</span>
+      ) : (
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1}
+          </strong>{' '}
+          of {pageLength}
+        </span>
+      )}
+    </div>
+  )
+}
+
+export default PageStatusFooter
