21762d9438f6db7d3ea8f129fa2964fdae5821b5
William Goh <wirriamm@gmail.com>
2022-10-14 12:05:51 +0800

    feat: api to get one GA with buckets virtuals

--- models/geneAnnotation.js
+++ models/geneAnnotation.js
@@ -1,8 +1,13 @@
 import { Schema, model, models, ObjectId } from "mongoose";
 
+import GeneAnnotationBucket from "./geneAnnotationBucket"
 
 const geneAnnotationSchema = new Schema(
   {
+    _id: {
+      $type: ObjectId,
+      required: true,
+    },
     type: {
       $type: String,
       required: true,
@@ -22,6 +27,15 @@ const geneAnnotationSchema = new Schema(
   }, { typeKey: "$type", strict: "throw" }
 )
 /* The schema will use the keyword $type to set the type of the field. And you can safely use type to set the name of the field. */
+
+geneAnnotationSchema.virtual("gene_annotation_buckets", {
+  ref: "GeneAnnotationBucket",
+  localField: "_id",
+  foreignField: "ga_id",
+})
+/* These are needed for virtual fields to appear */
+geneAnnotationSchema.set("toObject", { virtuals: true })
+geneAnnotationSchema.set("toJSON", { virtuals: true })
 
 const GeneAnnotation =
   models.GeneAnnotation ||
