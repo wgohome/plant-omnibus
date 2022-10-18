047af494a06e4259373f70951d51f630f58f13d6
William Goh <wirriamm@gmail.com>
2022-10-18 13:08:21 +0800

    feat: radio button to show all datapoints or just outliers

--- components/graphs/ExpressionBoxplot.tsx
+++ components/graphs/ExpressionBoxplot.tsx
@@ -2,12 +2,22 @@ import Plotly from "plotly.js"
 import React from "react"
 import Plot from "react-plotly.js"
 
+import Radio from "../atomic/inputs/Radio"
+
 const ExpressionBoxplot = ({ hideLoader, sampleAnnotations }) => {
   const downloadIcon = {
     width: 500,
     height: 600,
     path: 'M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM216 232V334.1l31-31c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-72 72c-9.4 9.4-24.6 9.4-33.9 0l-72-72c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l31 31V232c0-13.3 10.7-24 24-24s24 10.7 24 24z',
   }
+
+  /*
+    States:
+    Show all jitter points
+    Show only outliers
+    Show all jitter points, rescale to exclude outliers
+  */
+  const [ boxpoints, setBoxpoints ] = React.useState("all")  // "all" or "suspectedoutliers"
 
   return (
     <div className="relative my-4">
@@ -16,21 +26,30 @@ const ExpressionBoxplot = ({ hideLoader, sampleAnnotations }) => {
           sampleAnnotations.map(sa => ({
             type: "box",
             y: sa.tpms,
-            boxpoints: "all",
+            boxpoints: boxpoints,  // From state
+            jitter: 0.5,
             name: sa.name,
+            ids: sa.name,  // string[] for constancy of data points during animation
             showlegend: false,
-            opacity: 0.8,
-            fillcolor: "rgb(94, 126, 103)",
+            hoverinfo: "y",  // default: "all", but we hide the name here, bcos too repetitive
+            // hovertemplate: "",  // Refer to D3 formatting syntax if we need this in future
+            // yhoverformat: "",
+            fillcolor: "green",
+            opacity: 0.5,
             marker: {
               color: "rgb(84, 84, 84)",
-              opacity: 0.4,
-              size: 3,
-              // line: {
+              outliercolor: "rgba(172, 33, 33, 1)",
+              // opacity: 0.4,
+              size: 4,
+              line: {
               //   color: "rgb(220, 34, 164)",
-              // }
+                outliercolor: "rgba(220, 34, 164, 1)",
+              }
             },
             line: {
-              color: "rgb(132, 168, 142)"
+              // color: "green",
+              // color: "rgb(132, 168, 142)",
+              color: "rgb(144, 142, 142)",
             },
           }))
         }
@@ -76,6 +95,30 @@ const ExpressionBoxplot = ({ hideLoader, sampleAnnotations }) => {
         <p className="italic text-stone-500 text-sm">
           Boxplot reflects median TPM of each organ with raw data points as points to the left of each bar.
         </p>
+        <div className="mt-4 mb-8">
+          <Radio
+            groupName="boxplot-points-option"
+            radioOptions={[
+              { id: "all-points", label: "All datapoints" },
+              { id: "only-outliers", label: "Only outliers" },
+              { id: "crop-out-outliers", label: "All datapoints (axis scaled)" },
+            ]}
+            selected="all-points"
+            handleChange={(id) => {
+              switch (id) {
+                case "all-points":
+                case "crop-out-outliers":
+                  setBoxpoints("all")
+                  break
+                case "only-outliers":
+                  setBoxpoints("suspectedoutliers")
+                  break
+                default:
+                  break
+              }
+            }}
+          />
+        </div>
       </div>
     </div>
   )
