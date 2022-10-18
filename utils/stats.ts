3b6b18b373d66a2266a834e2b69a9dffbd08a791
William Goh <wirriamm@gmail.com>
2022-10-18 14:42:42 +0800

    feat: added math lib to find boxplot whiskers

--- utils/stats.ts
+++ utils/stats.ts
@@ -1,3 +1,5 @@
+import { quantileSeq } from "mathjs"
+
 export const roundDecimal = (value: number, decimalPoints: number = 3): number => {
   if (!decimalPoints || !Number.isInteger(decimalPoints)) {
     console.warn("decimalPoints must be an integer")
@@ -15,4 +17,17 @@ export const getStdDev = (values: number[]): number => {
   const variance: number = errSq.reduce((prev, curr) => prev + curr, 0) / values.length
   const rawStdDev: number = Math.sqrt(variance)
   return roundDecimal(rawStdDev, 3)
+}
+
+/*
+  Return [ topWhisker, bottomWhisker ]
+*/
+export const getWhiskers = (values: number[]): [number, number] => {
+  if (values.length == 0) { return [ 0, 0 ] }
+  const [ q1, q2, q3 ] = quantileSeq(values, [ 0.25, 0.5, 0.75 ], false)
+  const iqr = q3 - q1
+  const topWhisker = q3 + iqr
+  const bottomWhisker = Math.max(0, q1 - iqr)
+  // TODO: Can be more specific by giving the whisker as the highest point below q3 + iqr
+  return [ bottomWhisker, topWhisker ]
 }
