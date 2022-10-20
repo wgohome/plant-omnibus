11deba3bbe9ffa709d329bba77a540264224f7c9
William Goh <wirriamm@gmail.com>
2022-10-20 11:26:53 +0800

    refactor: TextLink component to take className instead of unconventional moreClassName

--- components/atomic/TextLink.tsx
+++ components/atomic/TextLink.tsx
@@ -3,19 +3,21 @@ import Link from "next/link"
 
 interface IProps {
   href: string
-  moreClassName?: string
+  className?: string
+  moreClassName?: string  // To be deprecated
   children: React.ReactNode
 }
 
 const TextLink: React.FC<IProps & {[key: string]: any}> = ({
   href,
-  moreClassName = "",
+  className,
+  moreClassName,
   children,
   ...moreAttributes  /* Eg: target="_blank" in the element */
 }) => {
   return (
     <Link href={href}>
-      <a className={`text-plb-green hover:underline active:text-plb-red visited:text-plb-green ${moreClassName}`} {...moreAttributes}>
+      <a className={`text-plb-green hover:underline active:text-plb-red ${moreClassName} ${className}`} {...moreAttributes}>
       {/* <a className={`text-plb-green hover:underline active:text-plb-red visited:text-plb-red ${moreClassName}`} {...moreAttributes}> */}
         {children}
       </a>
