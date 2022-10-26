4aebc44718caa70a63f09289ce2369c76582e716
William Goh <wirriamm@gmail.com>
2022-10-26 11:36:45 +0800

    feat: mapman index page have 3 tabs

--- components/tables/MapmanLevel1IndexTable.tsx
+++ components/tables/MapmanLevel1IndexTable.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 
+import TextLink from "../atomic/TextLink"
 import LocalPaginatedTable from "./generics/LocalPaginatedTable"
 
 type MapmanLevel1BinType = {
@@ -16,10 +17,20 @@ const MapmanLevel1IndexTable: React.FC<IProps> = ({ level1Bins }) => {
     {
       Header: "Mapman bin code",
       accessor: "bincode",
+      Cell: ({ value }) => (
+        <TextLink href={`/mapman/${value}/subbins`}>
+          {value}
+        </TextLink>
+      ),
     },
     {
       Header: "Mapman bin name",
       accessor: "binname",
+      Cell: ({ value, row }) => (
+        <TextLink href={`/mapman/${row.values.bincode}/subbins`}>
+          {value}
+        </TextLink>
+      ),
     },
   ], [])
 
--- pages/mapman/index.tsx
+++ pages/mapman/index.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Head from "next/head"
 import { GetServerSideProps, NextPage } from 'next'
 
+import { getGeneAnnotationsPage, getMapmanLevel1Bins } from '../../utils/geneAnnotations'
+
 import Layout from '../../components/Layout'
 import Header1 from '../../components/atomic/texts/Header1'
-import { getGeneAnnotationsPage, getMapmanLevel1Bins } from '../../utils/geneAnnotations'
 import MapmanIndexTable from '../../components/tables/MapmanIndexTable'
 import MapmanLevel1IndexTable from '../../components/tables/MapmanLevel1IndexTable'
+import { TabBodyGroup, TabBodyItem, TabGroup, TabHeaderGroup, TabHeaderItem } from '../../components/atomic/tabs'
 
 export const getServerSideProps: GetServerSideProps = async ({ params, query }) => {
   const level1Bins = await getMapmanLevel1Bins()
@@ -35,8 +37,36 @@ const MapmanIndexPage: NextPage<IProps> = ({ level1Bins, geneAnnotations, numGen
         <title>PFAM terms</title>
       </Head>
 
-      <Header1>Mapman terms (level 1)</Header1>
-      <MapmanLevel1IndexTable level1Bins={level1Bins} />
+      <Header1>Mapman bins</Header1>
+
+      <TabGroup>
+        <TabHeaderGroup>
+          <TabHeaderItem key="cards-level1" tabIndex={0}>
+            Level 1 bins (grid)
+          </TabHeaderItem>
+          <TabHeaderItem key="table-level1" tabIndex={1}>
+            Level 1 bins (table)
+          </TabHeaderItem>
+          <TabHeaderItem key="table-all" tabIndex={2}>
+            All Mapman bins
+          </TabHeaderItem>
+        </TabHeaderGroup>
+        <TabBodyGroup>
+          <TabBodyItem key="cards-level1" tabIndex={0}>
+            Cards index
+          </TabBodyItem>
+          <TabBodyItem key="table-level1" tabIndex={1}>
+            <MapmanLevel1IndexTable level1Bins={level1Bins} />
+          </TabBodyItem>
+          <TabBodyItem key="table-all" tabIndex={2}>
+            <MapmanIndexTable
+              initialGeneAnnotations={geneAnnotations}
+              pageTotal={numGeneAnnotations}
+            />
+          </TabBodyItem>
+        </TabBodyGroup>
+      </TabGroup>
+
     </Layout>
   )
 }
