2a8deafdbfa93194e00b7b4654af874fb2579549
William Goh <wirriamm@gmail.com>
2022-11-06 18:41:17 +0800

    feat: used flowbite for navbar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -18,10 +18,40 @@ export default function Layout({ children }) {
       </Head>
 
       <header>
-        <nav className="sticky w-full bg-plb-green text-white shadow-sm shadow-slate-200">
+        <nav className="sticky bg-plb-green py-4 pl-4 pr-2 md:px-2">
+          <div className="container flex flex-wrap justify-between items-center mx-auto">
+            <TextLink href="/" className="self-center whitespace-nowrap text-gray-50 text-xl font-medium">
+              Omnibus
+            </TextLink>
+            <button data-collapse-toggle="navbar-default" type="button" className="md:hidden inline-flex items-center p-2 ml-3 text-sm text-gray-50 rounded-lg focus:outline-none focus:ring-1 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
+              <span className="sr-only">Open main menu</span>
+              <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+            </button>
+            <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+              <ul className="flex flex-col md:flex-row bg-gray-50 md:bg-transparent rounded-xl border border-gray-200 md:border-0 md:space-x-6 py-0 mt-2 md:mt-0">
+                {[
+                  ["Species", "/species"],
+                  ["Organs", "/organs"],
+                  ["PFAM", "/interpro"],
+                  ["Mapman", "/mapman"],
+                ].map(([title, url]) => (
+                  <li key={title} className="hover:bg-gray-200 first:hover:rounded-t-xl last:hover:rounded-b-xl md:hover:bg-inherit">
+                    <TextLink
+                      href={url}
+                      className="block py-2 px-3 md:p-0 text-gray-700 md:text-gray-50 hover:font-medium md:hover:font-normal hover:no-underline md:hover:underline"
+                    >
+                      {title}
+                    </TextLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </nav>
+        {/* <nav className="sticky w-full bg-plb-green text-white shadow-sm shadow-slate-200">
           <div className="flex flex-row items-end py-4">
             <Link href="/">
-              <a className="grow text-2xl font-medium hover:underline active:text-blue-800 ml-8 mr-3">
+              <a className="grow text-2xl font-medium hover:underline active:text-black ml-6 mr-3">
                 Omnibus
               </a>
             </Link>
@@ -38,7 +68,7 @@ export default function Layout({ children }) {
               </Link>
             ))}
           </div>
-        </nav>
+        </nav> */}
       </header>
 
       <main className="container mx-auto py-4 px-8">{children}</main>
