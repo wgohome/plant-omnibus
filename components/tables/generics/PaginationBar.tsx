b3a7ddb7f7a735f1c2c3602be3fe0ff7e812345b
William Goh <wirriamm@gmail.com>
2022-11-22 12:46:23 +0800

    ui: make pagination bar not too tall

--- components/tables/generics/PaginationBar.tsx
+++ components/tables/generics/PaginationBar.tsx
@@ -12,70 +12,68 @@ const PaginationBar: React.FC = ({
   setPageSize,
 }) => {
   return (
-    // <nav className="flex justify-center my-3">
-      <div className="flex text-center drop-shadow">
-        <button
-          className="py-3 px-5 pl-6 text-gray-500 bg-white rounded-l-full border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
-          onClick={() => gotoPage(0)}
-          disabled={!canPreviousPage}
-        >
-          First
-        </button>
-        <button
-          className="py-3 px-5 text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
-          onClick={() => previousPage()}
-          disabled={!canPreviousPage}
-        >
-          {'<'}
-        </button>
-        <div
-          className="grow py-3 px-5 text-gray-500 bg-white border border-gray-300"
-        >
-          Go to{" "}
-          <input
-            className="text-center border border-stone-300"
-            type="number"
-            defaultValue={pageIndex + 1}
-            onChange={e => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0
-              gotoPage(page)
-            }}
-            style={{ width: '50px' }}
-          />
-        </div>
-        <div
-          className="grow py-3 px-5 text-gray-500 bg-white border border-gray-300"
-        >
-          <select
-            className="border border-stone-300"
-            value={pageSize}
-            onChange={e => {
-              setPageSize(Number(e.target.value))
-            }}
-          >
-            {[10, 20, 30, 40, 50].map(pageSize => (
-              <option key={pageSize} value={pageSize}>
-                Show {pageSize}
-              </option>
-            ))}
-          </select>
-        </div>
-        <button
-          className="py-3 px-5 text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
-          onClick={() => nextPage()}
-          disabled={!canNextPage}
-        >
-          {'>'}
-        </button>
-        <button
-          className="py-3 px-5 pr-6 text-gray-500 bg-white rounded-r-full border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
-          onClick={() => gotoPage(pageCount - 1)}
-          disabled={!canNextPage}
-        >
-          Last
-        </button>
+    <div className="flex text-center drop-shadow">
+      <button
+        className="py-3 px-5 pl-6 text-gray-500 bg-white rounded-l-full border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+        onClick={() => gotoPage(0)}
+        disabled={!canPreviousPage}
+      >
+        First
+      </button>
+      <button
+        className="py-3 px-5 text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+        onClick={() => previousPage()}
+        disabled={!canPreviousPage}
+      >
+        {'<'}
+      </button>
+      <div
+        className="grow py-1.5 px-5 text-gray-500 bg-white border border-gray-300"
+      >
+        Go to{" "}
+        <input
+          className="p-1 text-center border border-stone-200"
+          type="number"
+          defaultValue={pageIndex + 1}
+          onChange={e => {
+            const page = e.target.value ? Number(e.target.value) - 1 : 0
+            gotoPage(page)
+          }}
+          style={{ width: '80px' }}
+        />
       </div>
-    // </nav>
+      <div
+        className="grow py-1.5 px-5 text-gray-500 bg-white border border-gray-300"
+      >
+        <select
+          className="p-1 pr-6 border border-stone-200"
+          value={pageSize}
+          onChange={e => {
+            setPageSize(Number(e.target.value))
+          }}
+        >
+          {[10, 20, 30, 40, 50].map(pageSize => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button
+        className="py-3 px-5 text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+        onClick={() => nextPage()}
+        disabled={!canNextPage}
+      >
+        {'>'}
+      </button>
+      <button
+        className="py-3 px-5 pr-6 text-gray-500 bg-white rounded-r-full border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
+        onClick={() => gotoPage(pageCount - 1)}
+        disabled={!canNextPage}
+      >
+        Last
+      </button>
+    </div>
   )
 }
 
